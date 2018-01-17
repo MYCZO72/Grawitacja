c0ce66517e78b15dded10b3b86d9ce69129ad46f
MYCZO72 <michallmyczkowski@gmail.com>
2018-01-17 16:50:34 +0100

    smieszne

--- Grawitacja/Assets/Scripts/ProsteSterowanie.cs
+++ Grawitacja/Assets/Scripts/ProsteSterowanie.cs
@@ -5,6 +5,34 @@ using UnityEngine;
 public class ProsteSterowanie : MonoBehaviour
 {
 	public float speed;
+	public float accel;
+	public float zwolnij;
+	private Rigidbody2D rigidb;
+
+	void Awake () 
+	{
+		rigidb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate ()
+	{
+		float ruch = Input.GetAxis ("Horizontal");
+
+		if (ruch * rigidb.velocity.x < speed)
+			rigidb.AddForce(Vector2.right * ruch * accel);
+
+		if (Mathf.Abs (rigidb.velocity.x) > speed)
+			rigidb.velocity = new Vector2(Mathf.Sign (rigidb.velocity.x) * speed, rigidb.velocity.y);
+
+		if (ruch == 0) 
+		{
+			if(Mathf.Abs (rigidb.velocity.x) > 1f)
+				rigidb.AddForce(Vector2.right * Mathf.Sign (rigidb.velocity.x) * -1 * zwolnij);
+		}
+
+	}
+
+	/*
 	void Update ()
 	{
 		if( Input.GetKey( "up" ) )
@@ -15,13 +43,18 @@ public class ProsteSterowanie : MonoBehaviour
 		{
 			transform.Translate( 0, -speed, 0 );	
 		}
+
 		if( Input.GetKey( "right" ) )
 		{
-			transform.Translate( speed, 0, 0 );	
+			transform.Translate( speed*Time.deltaTime, 0, 0 );	
+		}
+		if( Input.GetKeyUp( "right" ) )
+		{
+			
 		}
 		if( Input.GetKey( "left" ) )
 		{
-			transform.Translate( -speed, 0, 0 );	
+			transform.Translate( -speed*Time.deltaTime, 0, 0 );	
 		}
-	}
+	}*/
 }
