a1fcb711e938bc6008a63b1b84a6826cfc3a7e81
kiczak <szymonkot1@gmail.com>
2018-01-17 15:14:47 +0100

    rusza sie

--- Grawitacja/Assets/Scripts/ProsteSterowanie.cs
+++ Grawitacja/Assets/Scripts/ProsteSterowanie.cs
@@ -2,25 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProsteSterowanie : MonoBehaviour {
-
+public class ProsteSterowanie : MonoBehaviour
+{
+	public float speed;
 	void Update ()
 	{
 		if( Input.GetKey( "up" ) )
 		{
-			transform.Translate( 0, 1, 0 );	
+			transform.Translate( 0, speed, 0 );	
 		}
 		if( Input.GetKey( "down" ) )
 		{
-			transform.Translate( 0, -1, 0 );	
+			transform.Translate( 0, -speed, 0 );	
 		}
 		if( Input.GetKey( "right" ) )
 		{
-			transform.Translate( 1, 0, 0 );	
+			transform.Translate( speed, 0, 0 );	
 		}
 		if( Input.GetKey( "left" ) )
 		{
-			transform.Translate( -1, 0, 0 );	
+			transform.Translate( -speed, 0, 0 );	
 		}
 	}
 }
